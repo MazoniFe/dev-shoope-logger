f015d4050e03c792e758eccc10f1387a89a55393
MazoniFe <lipemazonisilva@gmail.com>
2024-09-23 20:19:35 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux"; // Importando useDispatch
 import CallControlForm from "./assets/components/CallControlForm";
 import ProcessStatus from "./assets/components/ProcessStatus";
